3969bebdb6f3c05beea05b16e7c58c4abf5ec36c
Robin Fussell <rfussell17@gmail.com>
2021-03-23 17:00:51 -0700

    added html boilerplate text, css default styles text, sever code to js

--- boilerplate.js
+++ boilerplate.js
@@ -1,22 +1,65 @@
 const fs = require("fs");
 const folderName = process.argv[2] || "Project";
 
-try {
+const htmlBoiler = `
+  <!DOCTYPE html>
+  <html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Document</title>
+  </head>
+  <body>
+    <header>
+      <nav class="nav"></nav>
+    </header>
+    <main>
+    </main>
+    <footer></footer>
+    <script type="text/javascript" src="js/app.js"></script>
+  </body>
+  </html>
+ `;
 
+const serverBoiler = `
+  const express = require("express")
+  const app = express();
+
+
+
+  const port = 5000;
+  app.listen(port, () => console.log('listening on port ' + port));
+  `;
+
+const styleBoiler = `
+  *,
+  *::before,
+  *::after {
+    box-sizing: border-box;
+  }
+
+  :root {}
+
+  html {
+    scroll-behavior: smooth;
+  }
+
+  body {}
+  `;
+
+try {
   fs.mkdirSync(folderName);
-  fs.writeFileSync(`${folderName}/index.html`);
-  fs.writeFileSync(`${folderName}/projects.html`);
-  fs.writeFileSync(`${folderName}/project-item.html`);
+  fs.writeFileSync(`${folderName}/index.html`, htmlBoiler);
+  fs.writeFileSync(`${folderName}/projects.html`, htmlBoiler);
+  fs.writeFileSync(`${folderName}/project-item.html`, htmlBoiler);
   fs.mkdirSync(`${folderName}/css`);
-  fs.writeFileSync(`${folderName}/css/style.css`);
+  fs.writeFileSync(`${folderName}/css/style.css`, styleBoiler);
   fs.mkdirSync(`${folderName}/img`);
   fs.mkdirSync(`${folderName}/js`);
-  fs.writeFileSync(`${folderName}/js/index.js`);
-  fs.writeFileSync(`${folderName}/js/app.js`);
-
+  fs.writeFileSync(`${folderName}/js/app.js`, serverBoiler);
 } catch (e) {
-
   console.log("error");
   console.log(e);
-  
 }
